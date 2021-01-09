f4ccc6140998554fcfb7705d7c8072f68446c400
afeiship <1290657123@qq.com>
2021-01-09 16:25:32 +0800

    Release 1.0.4

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,8 @@
  * name: @jswork/next-weeks
  * description: Weeks for next.
  * homepage: https://github.com/afeiship/next-weeks
- * version: 1.0.3
- * date: 2021-01-06 22:45:22
+ * version: 1.0.4
+ * date: 2021-01-09 16:25:26
  * license: MIT
  */
 
